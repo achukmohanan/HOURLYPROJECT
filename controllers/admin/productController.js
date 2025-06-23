bf6dbc59d0dffdb24cbd7e17477826bd5209f31c
achukmohanan <achukmohanan06@gmail.com>
2025-06-23 23:01:04 +0530

    block and unblock products

--- controllers/admin/productController.js
+++ controllers/admin/productController.js
@@ -215,10 +215,32 @@ const removeProductOffer = async (req,res) =>{
     }
 }
 
+const blockProduct = async (req,res)=>{
+    try {
+        let id = req.query.id;
+        await Product.updateOne({_id:id},{$set:{isBlocked:true}});
+        res.redirect('/admin/products')
+    } catch (error) {
+        res.redirect('/pageerror')
+    }
+}
+
+const unblockProduct = async (req,res) =>{
+    try {
+        let id = req.query.id;
+        await Product.updateOne({_id:id},{$set:{isBlocked:false}})
+        res.redirect('/admin/products')
+    } catch (error) {
+        res.redirect('/pageerror')
+    }
+}
+
 module.exports = {
     getProductAddPage,
     addProducts,
     getAllProducts,
     addProductOffer,
-    removeProductOffer
+    removeProductOffer,
+    blockProduct,
+    unblockProduct
 }
