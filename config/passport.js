ef80f620dfa81685e5dbd80c93483a5d2522c63f
achu k <achukmohanan06@gmail.com>
2025-10-21 16:45:49 +0530

    google login issue resolved

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ const env = require('dotenv').config()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/google/callback"
+    callbackURL: "https://www.hourly.sbs/google/callback"
   
    
     
